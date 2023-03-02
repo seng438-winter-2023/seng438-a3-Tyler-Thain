a6aae8174dac33470665b5af04df425c6bce8919
JaredAssen <80238577+JaredAssen@users.noreply.github.com>
2023-03-02 16:51:54 -0700

    calculate row and column tests for datautilities

--- JFreeChart_Lab3/src/org/jfree/data/CalculateRowTotalTest.java
+++ JFreeChart_Lab3/src/org/jfree/data/CalculateRowTotalTest.java
@@ -15,60 +15,43 @@ public class CalculateRowTotalTest extends DataUtilities{
 	@Test
 	public void validInputTest() {
 		// This test covers a valid input for data and row (ECT)
-	    Mockery mockingContext = new Mockery();
-	    final Values2D values = mockingContext.mock(Values2D.class);
-	    mockingContext.checking(new Expectations() {
-	        {
-	            one(values).getColumnCount();
-	            will(returnValue(2));
-	            one(values).getValue(0, 0);
-	            will(returnValue(7.5));
-	            one(values).getValue(0, 1);
-	            will(returnValue(2.5));
-	        }
-	    });
+	
+	    //Mock does not work with EclEmma so test written with actual dependency 
+	    DefaultKeyedValues2D values = new DefaultKeyedValues2D();
+	    values.setValue(2.5, 0, 0);
+	    values.setValue(7.5, 0, 1);
 	    double result = DataUtilities.calculateRowTotal(values, 0);
 		assertEquals("Total should be 10 for valid input for index 0", 10, result, .000000001d);
 	}
 	
-	@Test(expected = InvalidParameterException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void nullDataTest() {
 		// This test covers an invalid input for data and a valid input for row (ECT)
 	    double actual = DataUtilities.calculateRowTotal(null, 1);
 	}
 	
-	@Test
+	@Test(expected = IndexOutOfBoundsException.class)
 	public void negativeIndexTest() {
 		// This test covers an invalid input for row and a valid input for data (BVT)
-		Mockery mockingContext = new Mockery();
-	    final Values2D values = mockingContext.mock(Values2D.class);
-	    mockingContext.checking(new Expectations() {
-	        {
-	            one(values).getColumnCount();
-	            will(returnValue(2));
-	            one(values).getValue(-1, 0);
-	            will(throwException(new IndexOutOfBoundsException()));
-	        }
-	    });
+		
+		//Mock does not work with EclEmma so test written with actual dependency 
+	    DefaultKeyedValues2D values = new DefaultKeyedValues2D();
+	    values.setValue(2.5, 0, 0);
+	    values.setValue(7.5, 0, 1);
+	    
 		double result = DataUtilities.calculateRowTotal(values, -1);
-		assertEquals("Total should be 0 for invalid input for index of -1", 0, result, .000000001d);
 	}
 	
-	@Test
+	@Test(expected = IndexOutOfBoundsException.class)
 	public void tooLargeIndexTest() {
 		// This test covers an invalid input for row and a valid input for data (BVT)
-		Mockery mockingContext = new Mockery();
-	    final Values2D values = mockingContext.mock(Values2D.class);
-	    mockingContext.checking(new Expectations() {
-	        {
-	            one(values).getColumnCount();
-	            will(returnValue(2));
-	            one(values).getValue(10, 0);
-	            will(throwException(new IndexOutOfBoundsException()));
-	        }
-	    });
+
+		//Mock does not work with EclEmma so test written with actual dependency 
+	    DefaultKeyedValues2D values = new DefaultKeyedValues2D();
+	    values.setValue(2.5, 0, 0);
+	    values.setValue(7.5, 0, 1);
 		double result = DataUtilities.calculateRowTotal(values, 10);
-		assertEquals("Total should be 0 for invalid input for index greater than the row size", 0, result, .000000001d);
+
 	}
 	
 
