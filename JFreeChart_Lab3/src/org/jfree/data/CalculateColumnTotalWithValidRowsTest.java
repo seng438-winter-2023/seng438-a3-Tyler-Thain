46704b1b3144767bbe88e2a896ff585f97bd29d4
JaredAssen <80238577+JaredAssen@users.noreply.github.com>
2023-03-03 10:54:36 -0700

    comments

--- JFreeChart_Lab3/src/org/jfree/data/CalculateColumnTotalWithValidRowsTest.java
+++ JFreeChart_Lab3/src/org/jfree/data/CalculateColumnTotalWithValidRowsTest.java
@@ -4,33 +4,34 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class CalculateRowTotalWithValidRowsTest extends DataUtilities{
+public class CalculateColumnTotalWithValidRowsTest extends DataUtilities {
 
 	@Test
 	public void validInputTest() {
-		//Tests valid input for calculateRowTotal
+		//Tests valid input for calculateColumnTotal
 		//Mock does not work with EclEmma so test written with actual dependency 
 		DefaultKeyedValues2D values = new DefaultKeyedValues2D();
 	    values.setValue(2.5, 0, 0);
-	    values.setValue(7.5, 0, 1);
-	    values.setValue(1.0, 0, 2);
+	    values.setValue(7.5, 1, 0);
+	    values.setValue(1.0, 2, 0);
 	    
 	    int[] i = {0,2};
-	    double result = DataUtilities.calculateRowTotal(values, 0, i);
+	    double result = DataUtilities.calculateColumnTotal(values, 0, i);
 		assertEquals("Total should be 3.5 for valid input for index 0", 3.5, result, .000000001d);
 	}
 	
 	@Test
 	public void tooManyValidRowsTest() {
-		//Tests what happens if validRows.size() > number of rows in data table for calculateRowTotal
+		//Tests what happens if validRows.size() > number of rows in data table
 		//Mock does not work with EclEmma so test written with actual dependency 
 		DefaultKeyedValues2D values = new DefaultKeyedValues2D();
 	    values.setValue(2.5, 0, 0);
-	    values.setValue(7.5, 0, 1);
-	    values.setValue(1.0, 0, 2);
+	    values.setValue(7.5, 1, 0);
+	    values.setValue(1.0, 2, 0);
 	    
 	    int[] i = {0,1,2,3};
-	    double result = DataUtilities.calculateRowTotal(values, 0, i);
+	    double result = DataUtilities.calculateColumnTotal(values, 0, i);
 		assertEquals("Total should be 11.0 for valid input for index 0", 11.0, result, .000000001d);
 	}
+
 }
